f494f181c31d165de8acbf88f1c438c14e2fdc1a
AI-liu <136758759@qq.com>
2019-06-19 17:11:24 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -4,6 +4,46 @@ from PIL import Image
 import numpy as np
 import os
 from torchvision import transforms
+import random
+import cv2
+
+
+train_dirs = [ "zurich/", "weimar/", "ulm/", "tubingen/", "stuttgart/",
+              "strasbourg/", "monchengladbach/", "krefeld/", "jena/","hanover/",
+              "hamburg/", "erfurt/", "dusseldorf/", "darmstadt/", "cologne/",
+              "bremen/", "bochum/", "aachen/"]
+val_dirs = ["frankfurt/", "lindau/", "munster/"]
+test_dirs = ["berlin/", "bielefeld/", "bonn/", "leverkusen/", "mainz/", "munich/"]
+
+
+
+
+
+def dataaug(input_tensor, label_tensor):
+    """use flip and warpAffine
+    """
+    inputs1 = input_tensor.numpy()
+    labels1 = label_tensor.numpy()
+    inputs1 = inputs1.transpose((1, 2, 0))
+
+    x = random.randint(-2, 2)
+    y = random.randint(-2, 2)
+    H = np.float32([[1,0,x],[0,1,y]])
+    inputs1 = cv2.warpAffine(inputs1,H,(inputs1.shape[1],inputs1.shape[0]))
+    labels1 = cv2.warpAffine(labels1.astype(np.float32),H,(inputs1.shape[1],inputs1.shape[0])).astype(np.uint8) 
+
+
+    if random.random() < 0.5:
+         inputs1 = cv2.flip(inputs1, 1) # horizontal flip
+         labels1 = cv2.flip(labels1, 1) # horizontal flip
+    
+    inputs1 = inputs1.transpose((2, 0, 1))
+    inputs1 = torch.from_numpy(inputs1)
+    labels1 = torch.from_numpy(labels1)
+
+    return inputs1, labels1
+
+
 
 
 
@@ -82,31 +122,45 @@ class CityScapeDataSet(torch.utils.data.Dataset):
                    8, 9, 10, 11, 12, 13, 14, 15, 16, 0, 0, 17, 18, 19, 0)
 
 
-    def __init__(self, data_path='/media//DATA/dataset/cityscapes'):
+    def __init__(self, data_path='/media/cicv/DATA/dataset/cityscapes'):
         self.data_path = data_path
-        self.image_path = '/gtFine_trainvaltest/leftImg8bit/train/aachen/'
-        self.label_path = '/gtFine_trainvaltest/gtFine/train/aachen/'
-        self.file_list = os.listdir(self.data_path + self.image_path)
+        self.image_path = '/gtFine_trainvaltest/leftImg8bit/train/'
+        self.label_path = '/gtFine_trainvaltest/gtFine/train/'
+
+        self.examples = []
+        for train_dir in train_dirs:
+            file_names = os.listdir(self.data_path + self.image_path  + train_dir)
+            for file_name in file_names:
+                example = {}
+                example["img_path"] = self.data_path + self.image_path  + train_dir + file_name
+                example["label_img_path"] = self.data_path + self.label_path + train_dir + file_name[:-15] + 'gtFine_labelIds.png'
+                self.examples.append(example)
 
 
     def __getitem__(self, i):
+        example = self.examples[i]
+        #print(example["img_path"])
+        #print(example["label_img_path"])
 
-        input_image = Image.open(self.data_path + self.image_path + self.file_list[i])
+        input_image = Image.open(example["img_path"])
         input_image = input_image.resize((int(input_image.width/2),int(input_image.height/2)),Image.NEAREST)
         preprocess = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),])
         input_tensor = preprocess(input_image)
 
-        label_image = Image.open(self.data_path + self.label_path + self.file_list[i][:-15] + 'gtFine_labelIds.png')
+        label_image = Image.open(example["label_img_path"])
         label_image = label_image.resize((int(label_image.width/2),int(label_image.height/2)),Image.NEAREST)
         # Remap class labels
         label_image = remap(label_image, self.full_classes, self.new_classes)
 
         label_tensor = PILToLongTensor()(label_image)
+
+        # Use augmentation
+        #input_tensor, label_tensor = dataaug(input_tensor, label_tensor)
         
         return  input_tensor , label_tensor
 
     def __len__(self):
-        return len(self.file_list)
+        return len(self.examples)
 
 if __name__ == "__main__":
     print("hello dataset")
@@ -114,6 +168,7 @@ if __name__ == "__main__":
     batch_size=12
     dataset=CityScapeDataSet()
     data_loader = data.DataLoader(dataset, batch_size, shuffle=True)
+
     for batch_idx , (input_tensor , label_tensor) in enumerate(data_loader):
         print(input_tensor[1].shape)
         print(label_tensor[1].shape)
