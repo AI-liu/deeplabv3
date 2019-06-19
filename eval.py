ab5129746559d2900bc3ace88b9ea684d4327371
AI-liu <136758759@qq.com>
2019-06-19 17:12:34 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,12 +1,124 @@
 import torch
-model = torch.load('deeplabv3.pth')
+import torch as F
+from PIL import Image
+import torchvision
+from torchvision import transforms
+from torchvision.transforms import ToPILImage
+from collections import OrderedDict
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Default encoding for pixel value, class name, and class color
+color_encoding = OrderedDict([
+            ('unlabeled', (0, 0, 0)),
+            ('road', (128, 64, 128)),
+            ('sidewalk', (244, 35, 232)),
+            ('building', (70, 70, 70)),
+            ('wall', (102, 102, 156)),
+            ('fence', (190, 153, 153)),
+            ('pole', (153, 153, 153)),
+            ('traffic_light', (250, 170, 30)),
+            ('traffic_sign', (220, 220, 0)),
+            ('vegetation', (107, 142, 35)),
+            ('terrain', (152, 251, 152)),
+            ('sky', (70, 130, 180)),
+            ('person', (220, 20, 60)),
+            ('rider', (255, 0, 0)),
+            ('car', (0, 0, 142)),
+            ('truck', (0, 0, 70)),
+            ('bus', (0, 60, 100)),
+            ('train', (0, 80, 100)),
+            ('motorcycle', (0, 0, 230)),
+            ('bicycle', (119, 11, 32))
+    ])
+
+
+class LongTensorToRGBPIL(object):
+    """Converts a ``torch.LongTensor`` to a ``PIL image``.
+
+    The input is a ``torch.LongTensor`` where each pixel's value identifies the
+    class.
+
+    Keyword arguments:
+    - rgb_encoding (``OrderedDict``): An ``OrderedDict`` that relates pixel
+    values, class names, and class colors.
+
+    """
+    def __init__(self, rgb_encoding):
+        self.rgb_encoding = rgb_encoding
+
+    def __call__(self, tensor):
+        """Performs the conversion from ``torch.LongTensor`` to a ``PIL image``
+
+        Keyword arguments:
+        - tensor (``torch.LongTensor``): the tensor to convert
+
+        Returns:
+        A ``PIL.Image``.
+
+        """
+        # Check if label_tensor is a LongTensor
+        if not isinstance(tensor, torch.LongTensor):
+            raise TypeError("label_tensor should be torch.LongTensor. Got {}"
+                            .format(type(tensor)))
+        # Check if encoding is a ordered dictionary
+        if not isinstance(self.rgb_encoding, OrderedDict):
+            raise TypeError("encoding should be an OrderedDict. Got {}".format(
+                type(self.rgb_encoding)))
+
+        # label_tensor might be an image without a channel dimension, in this
+        # case unsqueeze it
+        if len(tensor.size()) == 2:
+            tensor.unsqueeze_(0)
+
+        color_tensor = torch.ByteTensor(3, tensor.size(1), tensor.size(2))
+
+        for index, (class_name, color) in enumerate(self.rgb_encoding.items()):
+            # Get a mask of elements equal to index
+            mask = torch.eq(tensor, index).squeeze_()
+            # Fill color_tensor with corresponding colors
+            for channel, color_value in enumerate(color):
+                color_tensor[channel].masked_fill_(mask, color_value)
+
+        return ToPILImage()(color_tensor)
+
+
+
+
+
+def imshow_batch(images, labels):
+    """Displays two grids of images. The top grid displays ``images``
+    and the bottom grid ``labels``
+
+    Keyword arguments:
+    - images (``Tensor``): a 4D mini-batch tensor of shape
+    (B, C, H, W)
+    - labels (``Tensor``): a 4D mini-batch tensor of shape
+    (B, C, H, W)
+
+    """
+
+    # Make a grid with the images and labels and convert it to numpy
+    images = torchvision.utils.make_grid(images).numpy()
+    labels = torchvision.utils.make_grid(labels).numpy()
+
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(15, 7))
+    ax1.imshow(np.transpose(images, (1, 2, 0)))
+    ax2.imshow(np.transpose(labels, (1, 2, 0)))
+
+    plt.show()
+
+
+
+
+
+
+model = torch.load('deeplabv3_150.pth')
 model.eval()
 print(model)
 
-# sample execution (requires torchvision)
-from PIL import Image
-from torchvision import transforms
-#input_image = Image.open("/home/cicv/labelimage0320/labelimage/image/segmentation/1532589942135011547.png")
+
+
 input_image = Image.open("aachen_000000_000019_leftImg8bit.png")
 input_image = input_image.resize((int(input_image.width/2),int(input_image.height/2)),Image.NEAREST)
 preprocess = transforms.Compose([
@@ -24,20 +136,11 @@ if torch.cuda.is_available():
 
 with torch.no_grad():
     output = model(input_batch)[0]
-output_predictions = output.argmax(0)
-print(output_predictions)
+predictions = output.argmax(0)
 
-# create a color pallette, selecting a color for each class
-palette = torch.tensor([2 ** 25 - 1, 2 ** 15 - 1, 2 ** 21 - 1])
-colors = torch.as_tensor([i for i in range(21)])[:, None] * palette
-colors = (colors % 255).numpy().astype("uint8")
 
-# plot the semantic segmentation predictions of 21 classes in each color
-r = Image.fromarray(output_predictions.byte().cpu().numpy()).resize(input_image.size)
-r.putpalette(colors)
-
-import matplotlib.pyplot as plt
-plt.imshow(r)
-plt.show()
+label_to_rgb = transforms.Compose([ LongTensorToRGBPIL(color_encoding), transforms.ToTensor()])
+color_predictions = label_to_rgb(predictions.cpu())
+imshow_batch(transforms.ToTensor()(input_image).data.cpu(), color_predictions)
 
 
