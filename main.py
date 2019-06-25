efd93fe5592ff6f0ee9ff41ee482e95067c94974
AI-liu <136758759@qq.com>
2019-06-25 09:41:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import numpy as np
 
 from dataset import CityScapeDataSet
 from deeplabv3 import DeepLabV3
+from score import SegmentationMetric
 
 # dataset
 batch_size= 2
@@ -23,23 +24,33 @@ optimizer = optim.Adam(model.parameters())
 # define loss function
 loss_fn = nn.CrossEntropyLoss()
 
+# Metric
+metric = SegmentationMetric(model.num_classes)
 
-for epoch in range(200):
-    for batch_idx , (input_tensor , label_tensor) in enumerate(data_loader):
-        optimizer.zero_grad()
 
+for epoch in range(100):
+    metric.reset()
+
+    for step , (input_tensor , label_tensor) in enumerate(data_loader):
         input_tensor = Variable(input_tensor).cuda()
         label_tensor = torch.squeeze(label_tensor)
         label_tensor = Variable(label_tensor.long()).cuda()
+        
 
         output = model(input_tensor)
         loss = loss_fn(output,label_tensor)
-        print(loss)
+
+        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        if (epoch % 50 == 0):
-            torch.save(model, "deeplabv3_"+str(epoch)+".pth")
+        
+        metric.update(output, label_tensor)
+        pixAcc, mIoU = metric.get()
+        print("Sample: {:d}, Validation pixAcc: {:.3f}, mIoU: {:.3f}".format(step + 1, pixAcc, mIoU))
+
+    if (epoch % 10 == 0):
+        torch.save(model, "deeplabv3_"+str(epoch)+ "_mIoU_"+ str(mIoU)[:5] +".pth")
 
 
 
